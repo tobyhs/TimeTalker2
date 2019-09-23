5610428a176d58c79ef4086323406b6f06290a8f
Toby Hsieh <thrasher13b@gmail.com>
2019-09-22 17:50:30 -0700

    Add ServiceTestUtil.kt
    This contains a runService function for convenience to use Robolectric to start a
    Service and yield it.

---
+++ app/src/test/java/io/github/tobyhs/timetalker/test/ServiceTestUtil.kt
@@ -0,0 +1,22 @@
+package io.github.tobyhs.timetalker.test
+
+import android.app.Service
+
+import org.robolectric.Robolectric
+
+/**
+ * Starts a service, runs the given function on it, then destroys it.
+ *
+ * @param action function that takes a service so callers can make assertions on it
+ */
+inline fun <reified SC : Service> runService(action: (SC) -> Unit = { _ -> }): SC {
+    val controller = Robolectric.buildService(SC::class.java)
+    controller.create()
+    val service = controller.get()
+    try {
+        action(service)
+    } finally {
+        controller.destroy()
+    }
+    return service
+}
