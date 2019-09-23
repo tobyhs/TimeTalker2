c998e5e6469aa62cd0993c7eefbde7b44a06d745
Toby Hsieh <thrasher13b@gmail.com>
2019-09-22 20:34:59 -0700

    Create ShakeDetectorService
    This service speaks the time if it detects shaking.

---
+++ app/src/main/java/io/github/tobyhs/timetalker/ShakeDetectorService.kt
@@ -0,0 +1,77 @@
+package io.github.tobyhs.timetalker
+
+import android.app.Notification
+import android.app.Service
+import android.content.Intent
+import android.hardware.SensorManager
+import android.media.AudioManager
+import android.os.Handler
+import android.os.IBinder
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+
+import com.squareup.seismic.ShakeDetector
+
+import java.time.Clock
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * A service that speaks the time if shaking is detected
+ */
+class ShakeDetectorService : Service(), ShakeDetector.Listener {
+    internal var clock: Clock = Clock.systemDefaultZone()
+    internal lateinit var textToSpeech: TextToSpeech
+    private lateinit var shakeDetector: ShakeDetector
+
+    companion object {
+        const val NOTIFICATION_ID = 2
+        const val LIFETIME_MILLIS = 10000L
+        val TIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("h mm a")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        foregroundSelf()
+
+        textToSpeech = TextToSpeech(applicationContext) { status ->
+            if (status != TextToSpeech.SUCCESS) {
+                Toast.makeText(this, R.string.tts_init_failure, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        shakeDetector = ShakeDetector(this)
+        shakeDetector.start(getSystemService(SensorManager::class.java))
+
+        Handler().postDelayed({ stopSelf() }, LIFETIME_MILLIS)
+    }
+
+    override fun onDestroy() {
+        shakeDetector.stop()
+        textToSpeech.shutdown()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun hearShake() {
+        val audioManager = getSystemService(AudioManager::class.java)!!
+        if (audioManager.mode == AudioManager.MODE_NORMAL) {
+            val time = LocalTime.now(clock).format(TIME_FORMATTER)
+            textToSpeech.speak(time, TextToSpeech.QUEUE_FLUSH, null, time)
+        }
+    }
+
+    /**
+     * Makes this service run in the foreground with a notification
+     */
+    private fun foregroundSelf() {
+        val notification = Notification.Builder(this, App.SERVICE_NOTIFICATION_CHANNEL)
+            .setContentTitle(getString(R.string.shake_detector_notification_title))
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+        startForeground(NOTIFICATION_ID, notification)
+    }
+}
---
+++ app/src/test/java/io/github/tobyhs/timetalker/ShakeDetectorServiceTest.kt
@@ -0,0 +1,112 @@
+package io.github.tobyhs.timetalker
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Intent
+import android.media.AudioManager
+import android.speech.tts.TextToSpeech
+
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+import java.util.TimeZone
+import java.util.concurrent.TimeUnit
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.shadows.ShadowLooper
+import org.robolectric.shadows.ShadowToast
+
+import io.github.tobyhs.timetalker.test.runService
+
+@RunWith(RobolectricTestRunner::class)
+class ShakeDetectorServiceTest {
+    @Test
+    fun `onCreate foregrounds the service`() {
+        runService<ShakeDetectorService> { service ->
+            assertThat(shadowOf(service).isLastForegroundNotificationAttached, equalTo(true))
+
+            val notificationManager = service.getSystemService(NotificationManager::class.java)
+            val notifications = notificationManager!!.activeNotifications
+            assertThat(notifications.size, equalTo(1))
+
+            val notification = notifications.first()
+            val channelId = notification.notification.channelId
+            assertThat(channelId, equalTo(App.SERVICE_NOTIFICATION_CHANNEL))
+            val text = notification.notification.extras.getString(Notification.EXTRA_TITLE)
+            assertThat(text, equalTo("Detecting shaking"))
+            assertThat(notification.id, equalTo(ShakeDetectorService.NOTIFICATION_ID))
+        }
+    }
+
+    @Test
+    fun `shows a toast when TextToSpeech initialization fails`() {
+        runService<ShakeDetectorService> { service ->
+            ShadowToast.reset()
+            val listener = shadowOf(service.textToSpeech).onInitListener
+            listener.onInit(TextToSpeech.ERROR)
+            val lastToastText = ShadowToast.getTextOfLatestToast()
+            assertThat(lastToastText, equalTo("Time Talker: Failed to initialize TextToSpeech"))
+        }
+    }
+
+    @Test
+    fun `does not show a toast when TextToSpeech initialization succeeds` () {
+        runService<ShakeDetectorService> { service ->
+            ShadowToast.reset()
+            val listener = shadowOf(service.textToSpeech).onInitListener
+            listener.onInit(TextToSpeech.SUCCESS)
+            val lastToastText = ShadowToast.getTextOfLatestToast()
+            assertThat(lastToastText, nullValue())
+        }
+    }
+
+    @Test
+    fun `stops itself eventually`() {
+        val service = Robolectric.setupService(ShakeDetectorService::class.java)
+        ShadowLooper.pauseMainLooper()
+        Robolectric.getForegroundThreadScheduler().advanceBy(11, TimeUnit.SECONDS)
+        ShadowLooper.unPauseMainLooper()
+        assertThat(shadowOf(service).isStoppedBySelf, equalTo(true))
+    }
+
+    @Test
+    fun `onDestroy shuts down textToSpeech`() {
+        val service = runService<ShakeDetectorService>()
+        assertThat(shadowOf(service.textToSpeech).isShutdown, equalTo(true))
+    }
+
+    @Test
+    fun `onBind returns null`() {
+        val service = ShakeDetectorService()
+        assertThat(service.onBind(Intent()), nullValue())
+    }
+
+    @Test
+    fun `hearShake speaks the time`() {
+        runService<ShakeDetectorService> { service ->
+            TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+            service.clock = Clock.fixed(Instant.ofEpochSecond(1569182800), ZoneId.systemDefault())
+            service.hearShake()
+            assertThat(shadowOf(service.textToSpeech).lastSpokenText, equalTo("8 06 PM"))
+        }
+    }
+
+    @Test
+    fun `hearShake does not speak when the audio mode is not normal`() {
+        runService<ShakeDetectorService> { service ->
+            val audioManager = service.getSystemService(AudioManager::class.java)!!
+            audioManager.mode = AudioManager.MODE_IN_CALL
+            service.hearShake()
+            assertThat(shadowOf(service.textToSpeech).lastSpokenText, nullValue())
+        }
+    }
+}
