99a02cd5100732f9aa0a907da52682acff7be81c
Toby Hsieh <thrasher13b@gmail.com>
2019-12-22 11:52:39 -0800

    Add missing `@return` comment to .test.ServiceTestUtil.runService

--- app/src/test/java/io/github/tobyhs/timetalker/test/ServiceTestUtil.kt
+++ app/src/test/java/io/github/tobyhs/timetalker/test/ServiceTestUtil.kt
@@ -8,6 +8,7 @@ import org.robolectric.Robolectric
  * Starts a service, runs the given function on it, then destroys it.
  *
  * @param action function that takes a service so callers can make assertions on it
+ * @return instance of the service that ran
  */
 inline fun <reified SC : Service> runService(action: (SC) -> Unit = { _ -> }): SC {
     val controller = Robolectric.buildService(SC::class.java)
