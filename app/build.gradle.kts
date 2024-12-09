e2b23f473cb54dcbe2c89e3bf1978a287d0008b2
Toby Hsieh <thrasher13b@gmail.com>
2024-12-08 23:21:19 -0800

    Target Android SDK version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.github.tobyhs.timetalker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.tobyhs.timetalker"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -48,12 +48,12 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
     implementation("com.squareup:seismic:1.0.3")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.test:core:1.6.1")
     testImplementation("androidx.test.ext:junit:1.2.1")
-    testImplementation("org.robolectric:robolectric:4.11.1")
+    testImplementation("org.robolectric:robolectric:4.14.1")
 }
--- app/src/main/java/io/github/tobyhs/timetalker/MainActivity.kt
+++ app/src/main/java/io/github/tobyhs/timetalker/MainActivity.kt
@@ -2,8 +2,13 @@ package io.github.tobyhs.timetalker
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 
 /**
  * Activity that allows the user to start or stop {@code ScreenService}
@@ -12,6 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val contentView = findViewById<View>(R.id.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(contentView) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updateLayoutParams<MarginLayoutParams> {
+                topMargin = insets.top
+            }
+            WindowInsetsCompat.CONSUMED
+        }
 
         val enabledSwitch = findViewById<SwitchCompat>(R.id.enabled_switch)
         enabledSwitch.isChecked = ScreenService.isRunning
